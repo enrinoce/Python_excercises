379abfb20a7840441c920397803abc29e3e95865
enrinoce <enrinoce96@hotmail.it>
2025-04-14 17:05:05 +0200

    variables and strings; the print(f...)

---
+++ src/ex5.py
@@ -0,0 +1,17 @@
+name = 'Enrico'
+age = 28 # How old I am
+height = 181 # in cm
+weight = 73 #kilograms
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {name}")
+print(f"He's {height} cm tall")
+print(f"He's {weight} kilograms heavy")
+print(f"He's got {my_eyes} eyes and {my_hair} hair")
+print(f"Usually his teeth are {my_teeth}")
+print(f"{name} is a really good boy")
+
+total= age + height + weight
+print(f"If I add {age}, {height} and {weight}. I get {total}")
