7e2bd146a49faa4ae1f62855de4e479c805269cf
enrinoce <enrinoce96@hotmail.it>
2025-04-29 16:40:56 +0200

    three double-quotes and \n

---
+++ src/ex9.py
@@ -0,0 +1,14 @@
+
+days= "Mon Tue Wed Thu Fri Sat Sun"
+months= "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug" #the \n mette a capo l'elemento che viene dopo, quindi in questo caso vengono tutti i mesi in colonna
+
+print("Here are the days:", days)
+print("Here are the months:", months)
+
+#with three double-quotes possiamo scrivere quante righe vogliamo e quante parole vogliamo
+print("""
+There something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines, or 5, or 6.
+""") 
