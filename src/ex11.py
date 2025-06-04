77850e7841237d16077399902a2fef7652737612
enrinoce <enrinoce96@hotmail.it>
2025-06-04 18:37:12 +0200

    prime domande a python

--- src/ex11.py
+++ src/ex11.py
@@ -1,4 +1,4 @@
-print('How old are you?', end='')
+print('How old are you?', end='') #We put a end=' ' at the end of each print line. This tells print to not end the line with a newline character and go to the next line.
 age = input()
 print("How tall are you?", end='')
 height= input()
