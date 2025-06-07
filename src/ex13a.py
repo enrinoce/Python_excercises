b783a645713910d5c7e94e003ae490c5fa9fc089
enrinoce <enrinoce96@hotmail.it>
2025-06-07 11:46:28 +0200

    variante mia dell'esercizio con argv e input() combinati

---
+++ src/ex13a.py
@@ -0,0 +1,7 @@
+from sys import argv
+trippi, troppi, trulimero, trulicina = argv #argomenti ex13a.py bene volo sempre, primo input alto, secondo input JM
+
+print("exercise number:", trippi)
+print("Come stai?", troppi, trulicina)
+print("Domani che fai?", trulimero, input())
+print("Forza Viola", trulicina, input())
